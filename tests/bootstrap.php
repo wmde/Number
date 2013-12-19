bf7752a5469d556a43e4ab070c0c0fdad38d7cf0
jeroendedauw <jeroendedauw@gmail.com>
2013-12-19 18:24:56 +0100

    Make sure PHPUnit bootstrap commands get executed from the correct dir and from cli

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,12 @@
 <?php
 
-echo exec( 'composer update' ) . "\n";
+if ( php_sapi_name() !== 'cli' ) {
+	die( 'Not an entry point' );
+}
+
+$pwd = exec( 'pwd' );
+chdir( __DIR__ . '/..' );
+echo passthru( 'composer update' ) . "\n";
+chdir( $pwd );
 
 require_once( __DIR__ . '/../vendor/autoload.php' );

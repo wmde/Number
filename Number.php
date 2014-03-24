2369726a48dafa2c8c1cb03dec156620fcf05d22
Jeroen De Dauw <jeroendedauw@gmail.com>
2014-03-24 17:18:50 +0100

    Update Number.php

--- Number.php
+++ Number.php
@@ -11,11 +11,11 @@
  */
 
 if ( defined( 'DATAVALUES_NUMBER_VERSION' ) ) {
-	// Do not initialize more then once.
+	// Do not initialize more than once.
 	return 1;
 }
 
-define( 'DATAVALUES_NUMBER_VERSION', '0.4' );
+define( 'DATAVALUES_NUMBER_VERSION', '0.4 alpha' );
 
 if ( defined( 'MEDIAWIKI' ) ) {
 	$GLOBALS['wgExtensionCredits']['datavalues'][] = array(

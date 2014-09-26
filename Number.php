c24d0296219a768b48580eaf1e017a4b07615e94
Marius Hoch <hoo@online.de>
2014-09-26 03:36:38 +0200

    Specify 'license-name' => 'GPL-2.0+' for MediaWiki

--- Number.php
+++ Number.php
@@ -27,5 +27,6 @@ if ( defined( 'MEDIAWIKI' ) ) {
 		),
 		'url' => 'https://github.com/DataValues/Number',
 		'description' => 'Numerical value objects, parsers and formatters',
+		'license-name' => 'GPL-2.0+'
 	);
 }

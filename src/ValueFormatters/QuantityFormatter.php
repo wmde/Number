99e28aa0afde5aebce920ccc1ebf237b89a1a8ec
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2015-09-04 13:27:26 +0200

    Add more rounding tests for QuantityFormatter

--- src/ValueFormatters/QuantityFormatter.php
+++ src/ValueFormatters/QuantityFormatter.php
@@ -237,7 +237,6 @@ class QuantityFormatter extends ValueFormatterBase {
 	 */
 	private function formatMargin( DecimalValue $margin, $roundingExponent ) {
 		if ( $this->options->getOption( self::OPT_SHOW_UNCERTAINTY_MARGIN ) ) {
-			// TODO: never round to 0! See bug #56892
 			$roundedMargin = $this->decimalMath->roundToExponent( $margin, $roundingExponent );
 
 			if ( !$roundedMargin->isZero() ) {

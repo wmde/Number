2e126eee1c0067c6c0f35b4fae0388ff11725307
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2016-07-12 16:00:03 +0200

    Only add UnboundedQuantityValue

---
+++ src/DataValues/UnboundedQuantityValue.php
@@ -0,0 +1,283 @@
+<?php
+
+namespace DataValues;
+
+use InvalidArgumentException;
+
+/**
+ * Class representing a quantity with associated unit.
+ * The amount is stored as a @see DecimalValue object.
+ *
+ * @see QuantityValue for quantities with a known uncertainty interval.
+ * For simple numeric amounts use @see NumberValue.
+ *
+ * @note UnboundedQuantityValue and QuantityValue both use the value type ID "quantity".
+ * The fact that we use subclassing to model the bounded vs the unbounded case should be
+ * considered an implementation detail.
+ *
+ * @since 0.8
+ *
+ * @license GPL-2.0+
+ * @author Daniel Kinzler
+ */
+class UnboundedQuantityValue extends DataValueObject {
+
+	/**
+	 * The quantity's amount
+	 *
+	 * @var DecimalValue
+	 */
+	protected $amount;
+
+	/**
+	 * The quantity's unit identifier (use "1" for unitless quantities).
+	 *
+	 * @var string
+	 */
+	protected $unit;
+
+	/**
+	 * @param DecimalValue $amount
+	 * @param string $unit A unit identifier. Must not be empty, use "1" for unit-less quantities.
+	 *
+	 * @throws IllegalValueException
+	 */
+	public function __construct( DecimalValue $amount, $unit ) {
+		if ( !is_string( $unit ) || $unit === '' ) {
+			throw new IllegalValueException( '$unit must be a non-empty string. Use "1" for unit-less quantities.' );
+		}
+
+		$this->amount = $amount;
+		$this->unit = $unit;
+	}
+
+	/**
+	 * Returns a QuantityValue representing the given amount.
+	 *
+	 * This is a convenience wrapper around the constructor that accepts native values
+	 * instead of DecimalValue objects.
+	 *
+	 * @note: if the amount or a bound is given as a string, the string must conform
+	 * to the rules defined by @see DecimalValue.
+	 *
+	 * @param string|int|float|DecimalValue $amount
+	 * @param string $unit A unit identifier. Must not be empty, use "1" for unit-less quantities.
+	 *
+	 * @return self
+	 * @throws IllegalValueException
+	 */
+	public static function newFromNumber( $amount, $unit = '1' ) {
+		$amount = self::asDecimalValue( 'amount', $amount );
+
+		return new self( $amount, $unit );
+	}
+
+	/**
+	 * Converts $number to a DecimalValue if possible and necessary.
+	 *
+	 * @note: if the $number is given as a string, it must conform to the rules
+	 *        defined by @see DecimalValue.
+	 *
+	 * @param string $name The variable name to use in exception messages
+	 * @param string|int|float|DecimalValue|null $number
+	 * @param DecimalValue|null $default
+	 *
+	 * @throws IllegalValueException
+	 * @throws InvalidArgumentException
+	 * @return DecimalValue
+	 */
+	protected static function asDecimalValue( $name, $number, DecimalValue $default = null ) {
+		if ( !is_string( $name ) ) {
+			throw new InvalidArgumentException( '$name must be a string' );
+		}
+
+		if ( $number === null ) {
+			if ( $default === null ) {
+				throw new InvalidArgumentException( '$' . $name . ' must not be null' );
+			}
+
+			$number = $default;
+		}
+
+		if ( $number instanceof DecimalValue ) {
+			// nothing to do
+		} elseif ( is_int( $number ) || is_float( $number ) || is_string( $number ) ) {
+			$number = new DecimalValue( $number );
+		} else {
+			throw new IllegalValueException( '$' . $name . '  must be a string, int, or float' );
+		}
+
+		return $number;
+	}
+
+	/**
+	 * @see Serializable::serialize
+	 *
+	 * @return string
+	 */
+	public function serialize() {
+		return serialize( array(
+			$this->amount,
+			$this->unit
+		) );
+	}
+
+	/**
+	 * @see Serializable::unserialize
+	 *
+	 * @param string $data
+	 */
+	public function unserialize( $data ) {
+		list( $amount, $unit ) = unserialize( $data );
+		$this->__construct( $amount, $unit );
+	}
+
+	/**
+	 * @see DataValue::getType
+	 *
+	 * @return string
+	 */
+	public static function getType() {
+		return 'quantity';
+	}
+
+	/**
+	 * @see DataValue::getSortKey
+	 *
+	 * @return float
+	 */
+	public function getSortKey() {
+		return $this->amount->getValueFloat();
+	}
+
+	/**
+	 * Returns the quantity object.
+	 * @see DataValue::getValue
+	 *
+	 * @return self
+	 */
+	public function getValue() {
+		return $this;
+	}
+
+	/**
+	 * Returns the amount represented by this quantity.
+	 *
+	 * @return DecimalValue
+	 */
+	public function getAmount() {
+		return $this->amount;
+	}
+
+	/**
+	 * Returns the unit held by this quantity.
+	 * Unit-less quantities should use "1" as their unit.
+	 *
+	 * @return string
+	 */
+	public function getUnit() {
+		return $this->unit;
+	}
+
+	/**
+	 * Returns a transformed value derived from this QuantityValue by applying
+	 * the given transformation to the amount and the upper and lower bounds.
+	 * The resulting amount and bounds are rounded to the significant number of
+	 * digits. Note that for exact quantities (with at least one bound equal to
+	 * the amount), no rounding is applied (since they are considered to have
+	 * infinite precision).
+	 *
+	 * The transformation is provided as a callback, which must implement a
+	 * monotonously increasing, fully differentiable function mapping a DecimalValue
+	 * to a DecimalValue. Typically, it will be a linear transformation applying a
+	 * factor and an offset.
+	 *
+	 * @param string $newUnit The unit of the transformed quantity.
+	 *
+	 * @param callable $transformation A callback that implements the desired transformation.
+	 *        The transformation will be called three times, once for the amount, once
+	 *        for the lower bound, and once for the upper bound. It must return a DecimalValue.
+	 *        The first parameter passed to $transformation is the DecimalValue to transform
+	 *        In addition, any extra parameters passed to transform() will be passed through
+	 *        to the transformation callback.
+	 *
+	 * @param mixed ... Any extra parameters will be passed to the $transformation function.
+	 *
+	 * @todo Should be factored out into a separate QuantityMath class.
+	 *
+	 * @throws InvalidArgumentException
+	 * @return self
+	 */
+	public function transform( $newUnit, $transformation ) {
+		if ( !is_callable( $transformation ) ) {
+			throw new InvalidArgumentException( '$transformation must be callable.' );
+		}
+
+		if ( !is_string( $newUnit ) || $newUnit === '' ) {
+			throw new InvalidArgumentException( '$newUnit must be a non-empty string. Use "1" for unit-less quantities.' );
+		}
+
+		// Apply transformation by calling the $transform callback.
+		// The first argument for the callback is the DataValue to transform. In addition,
+		// any extra arguments given for transform() are passed through.
+		$args = func_get_args();
+		array_shift( $args );
+
+		$args[0] = $this->amount;
+		$amount = call_user_func_array( $transformation, $args );
+
+		return new self( $amount, $newUnit );
+	}
+
+	public function __toString() {
+		return $this->amount->getValue()
+			. ( $this->unit === '1' ? '' : $this->unit );
+	}
+
+	/**
+	 * @see DataValue::getArrayValue
+	 *
+	 * @return array
+	 */
+	public function getArrayValue() {
+		return array(
+			'amount' => $this->amount->getArrayValue(),
+			'unit' => $this->unit,
+		);
+	}
+
+	/**
+	 * Constructs a new instance of the DataValue from the provided data.
+	 * This can round-trip with @see getArrayValue
+	 *
+	 * @param mixed $data
+	 *
+	 * @return self
+	 * @throws IllegalValueException
+	 */
+	public static function newFromArray( $data ) {
+		self::requireArrayFields( $data, array( 'amount', 'unit' ) );
+
+		return new static(
+			DecimalValue::newFromArray( $data['amount'] ),
+			$data['unit']
+		);
+	}
+
+	/**
+	 * @see Comparable::equals
+	 *
+	 * @param mixed $target
+	 *
+	 * @return bool
+	 */
+	public function equals( $target ) {
+		if ( $this === $target ) {
+			return true;
+		}
+
+		return $target instanceof self
+			&& $this->toArray() === $target->toArray();
+	}
+
+}
---
+++ tests/DataValues/UnboundedQuantityValueTest.php
@@ -0,0 +1,235 @@
+<?php
+
+namespace DataValues\Tests;
+
+use DataValues\DataValue;
+use DataValues\DecimalValue;
+use DataValues\IllegalValueException;
+use DataValues\UnboundedQuantityValue;
+
+/**
+ * @covers DataValues\UnboundedQuantityValue
+ *
+ * @group DataValue
+ * @group DataValueExtensions
+ *
+ * @license GPL-2.0+
+ * @author Daniel Kinzler
+ */
+class UnboundedQuantityValueTest extends DataValueTest {
+
+	/**
+	 * @see DataValueTest::getClass
+	 *
+	 * @return string
+	 */
+	public function getClass() {
+		return 'DataValues\UnboundedQuantityValue';
+	}
+
+	public function validConstructorArgumentsProvider() {
+		$argLists = array();
+
+		$argLists[] = array( new DecimalValue( '+42' ), '1' );
+		$argLists[] = array( new DecimalValue( '+0.01' ), '1' );
+		$argLists[] = array( new DecimalValue( '-0.5' ), '1' );
+
+		return $argLists;
+	}
+
+	public function invalidConstructorArgumentsProvider() {
+		$argLists = array();
+
+		$argLists[] = array( new DecimalValue( '+0' ), '' );
+		$argLists[] = array( new DecimalValue( '+0' ), 1 );
+
+		return $argLists;
+	}
+
+	/**
+	 * @dataProvider instanceProvider
+	 */
+	public function testGetValue( UnboundedQuantityValue $quantity, array $arguments ) {
+		$this->assertInstanceOf( $this->getClass(), $quantity->getValue() );
+	}
+
+	/**
+	 * @dataProvider instanceProvider
+	 */
+	public function testGetAmount( UnboundedQuantityValue $quantity, array $arguments ) {
+		$this->assertEquals( $arguments[0], $quantity->getAmount() );
+	}
+
+	/**
+	 * @dataProvider instanceProvider
+	 */
+	public function testGetUnit( UnboundedQuantityValue $quantity, array $arguments ) {
+		$this->assertEquals( $arguments[1], $quantity->getUnit() );
+	}
+
+	/**
+	 * @dataProvider newFromNumberProvider
+	 */
+	public function testNewFromNumber( $amount, $unit, UnboundedQuantityValue $expected ) {
+		$quantity = UnboundedQuantityValue::newFromNumber( $amount, $unit );
+
+		$this->assertEquals( $expected->getAmount()->getValue(), $quantity->getAmount()->getValue() );
+	}
+
+	public function newFromNumberProvider() {
+		return array(
+			array(
+				42, '1',
+				new UnboundedQuantityValue( new DecimalValue( '+42' ), '1' )
+			),
+			array(
+				-0.05, '1',
+				new UnboundedQuantityValue( new DecimalValue( '-0.05' ), '1' )
+			),
+			array(
+				0, 'm',
+				new UnboundedQuantityValue( new DecimalValue( '+0' ), 'm' )
+			),
+			array(
+				'+23', '1',
+				new UnboundedQuantityValue( new DecimalValue( '+23' ), '1' )
+			),
+			array(
+				'+42', '1',
+				new UnboundedQuantityValue( new DecimalValue( '+42' ), '1' )
+			),
+			array(
+				'-0.05', 'm',
+				new UnboundedQuantityValue( new DecimalValue( '-0.05' ), 'm' )
+			),
+			array(
+				new DecimalValue( '+42' ), '1',
+				new UnboundedQuantityValue( new DecimalValue( '+42' ), '1' )
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider validArraySerializationProvider
+	 */
+	public function testNewFromArray( $data, DataValue $expected ) {
+		$value = UnboundedQuantityValue::newFromArray( $data );
+		$this->assertTrue( $expected->equals( $value ), $value . ' should equal ' . $expected );
+	}
+
+	public function validArraySerializationProvider() {
+		return array(
+			'unbounded' => array(
+				array(
+					'amount' => '+2',
+					'unit' => '1',
+				),
+				UnboundedQuantityValue::newFromNumber( '+2', '1' )
+			),
+			'with-extra' => array(
+				array(
+					'amount' => '+2',
+					'unit' => '1',
+					'upperBound' => '+2.5',
+					'lowerBound' => '+1.5',
+				),
+				UnboundedQuantityValue::newFromNumber( '+2', '1' )
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider invalidArraySerializationProvider
+	 */
+	public function testNewFromArray_failure( $data ) {
+		$this->setExpectedException( 'DataValues\IllegalValueException' );
+		UnboundedQuantityValue::newFromArray( $data );
+	}
+
+	public function invalidArraySerializationProvider() {
+		return array(
+			'no-amount' => array(
+				array(
+					'unit' => '1',
+				)
+			),
+			'no-unit' => array(
+				array(
+					'amount' => '+2',
+				)
+			),
+			'bad-amount' => array(
+				array(
+					'amount' => 'x',
+					'unit' => '1',
+				)
+			),
+		);
+	}
+
+	/**
+	 * @see https://phabricator.wikimedia.org/T110728
+	 * @see http://www.regular-expressions.info/anchors.html#realend
+	 */
+	public function testTrailingNewlineRobustness() {
+		$value = UnboundedQuantityValue::newFromArray( array(
+			'amount' => "-0.0\n",
+			'unit' => "1\n",
+		) );
+
+		$this->assertSame( array(
+			'amount' => '+0.0',
+			'unit' => "1\n",
+		), $value->getArrayValue() );
+	}
+
+	/**
+	 * @dataProvider instanceProvider
+	 */
+	public function testGetSortKey( UnboundedQuantityValue $quantity ) {
+		$this->assertEquals( $quantity->getAmount()->getValueFloat(), $quantity->getSortKey() );
+	}
+
+	/**
+	 * @dataProvider transformProvider
+	 */
+	public function testTransform( UnboundedQuantityValue $quantity, $transformation, UnboundedQuantityValue $expected ) {
+		$args = func_get_args();
+		$extraArgs = array_slice( $args, 3 );
+
+		$call = array( $quantity, 'transform' );
+		$callArgs = array_merge( array( 'x', $transformation ), $extraArgs );
+		$actual = call_user_func_array( $call, $callArgs );
+
+		$this->assertEquals( 'x', $actual->getUnit() );
+		$this->assertEquals( $expected->getAmount()->getValue(), $actual->getAmount()->getValue(), 'value' );
+	}
+
+	public function transformProvider() {
+		$identity = function ( DecimalValue $value ) {
+			return $value;
+		};
+
+		$square = function ( DecimalValue $value ) {
+			$v = $value->getValueFloat();
+			return new DecimalValue( $v * $v * $v );
+		};
+
+		$scale = function ( DecimalValue $value, $factor ) {
+			return new DecimalValue( $value->getValueFloat() * $factor );
+		};
+
+		return array(
+			 0 => array( UnboundedQuantityValue::newFromNumber( '+10', '1' ), $identity, UnboundedQuantityValue::newFromNumber( '+10', '?' ) ),
+			 1 => array( UnboundedQuantityValue::newFromNumber( '-0.5', '1' ), $identity, UnboundedQuantityValue::newFromNumber( '-0.5', '?' ) ),
+			 2 => array( UnboundedQuantityValue::newFromNumber( '+0', '1' ), $square,   UnboundedQuantityValue::newFromNumber( '+0', '?' ) ),
+			 3 => array( UnboundedQuantityValue::newFromNumber( '+10', '1' ), $square,   UnboundedQuantityValue::newFromNumber( '+1000', '?' ) ), // note how rounding applies to bounds
+			 4 => array( UnboundedQuantityValue::newFromNumber( '+0.5', '1' ), $scale,    UnboundedQuantityValue::newFromNumber( '+0.25', '?' ), 0.5 ),
+
+			// note: absolutely exact values require conversion with infinite precision!
+			10 => array( UnboundedQuantityValue::newFromNumber( '+100', '1' ), $scale, UnboundedQuantityValue::newFromNumber( '+12825.0', '?' ), 128.25 ),
+			13 => array( UnboundedQuantityValue::newFromNumber( '+100', '1' ), $scale, UnboundedQuantityValue::newFromNumber( '+333.33', '?' ), 3.3333 ),
+		);
+	}
+
+}
